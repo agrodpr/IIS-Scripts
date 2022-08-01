1d84cfcbe4247c56e7f47d637bdcea24345c2e49
agrodpr <110069903+agrodpr@users.noreply.github.com>
2022-08-01 16:15:05 -0400

    Add files via upload

---
+++ IIS_Bindings.ps1
@@ -0,0 +1,12 @@
+﻿#IISSiteName is the name of the site in IIS
+$IISSiteName="sub.domain.com"
+#IISURL is the URL you want to add a binding for
+$IISURL="sub.domain.com"
+
+#Get current site bindings. You can use filters for ports, SSL Flags
+Get-IISSiteBinding $IISSiteName # "*:80:" "*:443:"
+ 
+#Creates the HTTPS Binding with SSL FLag 1 for SNI
+New-WebBinding -Name $InternalSite -sslFlags 1 -Protocol https -IP * -Port 443 -HostHeader $IISURL
+#Creates the HTTP that is necessary to create HTTP Rewrite rules to redirect non HTTPS requests
+New-WebBinding -Name $InternalSite -Protocol http -IP * -Port 80 -HostHeader $IISURL
